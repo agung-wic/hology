1a43463ce611c7df26c62067bd4a1c656f430f2f
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-08 19:01:59 +0700

    coba pasien view

--- resources/views/master/dashboard.blade.php
+++ resources/views/master/dashboard.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>ALIVE</title>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 
   <!-- Custom fonts for this template-->
