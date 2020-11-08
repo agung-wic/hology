b8f58b045e4136f1a84f27ac32c30b769737df6b
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-09 02:46:14 +0700

    debug

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -11,15 +11,15 @@ class ChartController extends Controller
         $dataset = \App\Dataset::all();
         dd(json_encode($dataset));
 
-        $result = mysqli_query($mysqli, $dataset);
-        $data1 = '';
-        $data2 = '';
-        while ($row = mysqli_fetch_array($result)) {
-            $data1 = $data1 . '"' . $row['data1'] . '",';
-            $data2 = $data2 . '"' . $row['data2'] . '",';
-        }
-        $data1 = trim($data1, ",");
-        $data2 = trim($data2, ",");
+        // $result = mysqli_query($mysqli, $dataset);
+        // $data1 = '';
+        // $data2 = '';
+        // while ($row = mysqli_fetch_array($result)) {
+        //     $data1 = $data1 . '"' . $row['data1'] . '",';
+        //     $data2 = $data2 . '"' . $row['data2'] . '",';
+        // }
+        // $data1 = trim($data1, ",");
+        // $data2 = trim($data2, ",");
 
 
         return view('chart', ['dataset' => $dataset], ['data1' => $data1], ['data2' => $data2]);
