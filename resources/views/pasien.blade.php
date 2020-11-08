a36d73c01c79d55d77f5cb4a5536695c9240e330
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-08 18:47:28 +0700

    coba pasien view

--- resources/views/pasien.blade.php
+++ resources/views/pasien.blade.php
@@ -1,76 +1,73 @@
 @extends('master.dashboard')
 
 @section('content')
-<div id="content">
-
-  <!-- Begin Page Content -->
-  <div class="container-fluid">
-
-    <!-- Content Row -->
-    <div class="row">
-      <!-- Earnings (Monthly) Card Example -->
-      <div class="col-12 mb-4">
-        <div class="card-group">
-          <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-3">
-                Dr. Bambang Hardoyo
-              </div>
-              <h5 class="card-title">Laporan Medical Check Up</h5>
-              <p class="card-text" style="margin-bottom:-0.1%">Berikut adalah hasil check up : </p>
-              <p class="card-text" style="margin-bottom:-0.1%">Nama : Ahmad Zakiy Mulyanto</p>
-              <p class="card-text" style="margin-bottom:-0.1%">Usia : 21 Tahun</p>
-              <p class="card-text" style="margin-bottom:-0.1%">No. KTP : 12345678</p>
-              <p class="card-text" style="margin-bottom:1%">No. Surat : 2</p>
-              <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
-                <i class="fas fa-download fa-sm text-white-50"></i> Download
-              </a>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+  <!-- Content Row -->
+  <div class="row">
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-12 mb-4">
+      <div class="card-group">
+        <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-3">
+              Dr. Bambang Hardoyo
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Selasa, 3 November 2020</small>
-            </div>
+            <h5 class="card-title">Laporan Medical Check Up</h5>
+            <p class="card-text" style="margin-bottom:-0.1%">Berikut adalah hasil check up : </p>
+            <p class="card-text" style="margin-bottom:-0.1%">Nama : Ahmad Zakiy Mulyanto</p>
+            <p class="card-text" style="margin-bottom:-0.1%">Usia : 21 Tahun</p>
+            <p class="card-text" style="margin-bottom:-0.1%">No. KTP : 12345678</p>
+            <p class="card-text" style="margin-bottom:1%">No. Surat : 2</p>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+              <i class="fas fa-download fa-sm text-white-50"></i> Download
+            </a>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Selasa, 3 November 2020</small>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <div class="row">
-      <!-- Earnings (Monthly) Card Example -->
-      <div class="col-12 mb-4">
-        <div class="card-group">
-          <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-3">
-                Dr. Bambang Hardoyo
-              </div>
-              <h5 class="card-title">Laporan Medical Check Up</h5>
-              <p class="card-text" style="margin-bottom:-0.1%">Berikut adalah hasil check up : </p>
-              <p class="card-text" style="margin-bottom:-0.1%">Nama : Ahmad Zakiy Mulyanto</p>
-              <p class="card-text" style="margin-bottom:-0.1%">Usia : 21 Tahun</p>
-              <p class="card-text" style="margin-bottom:-0.1%">No. KTP : 12345678</p>
-              <p class="card-text" style="margin-bottom:1%">No. Surat : 1</p>
-              <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
-                <i class="fas fa-download fa-sm text-white-50"></i> Download
-              </a>
+  <div class="row">
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-12 mb-4">
+      <div class="card-group">
+        <div class="card border-left-success shadow h-100 py-2">
+          <div class="card-body">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-3">
+              Dr. Bambang Hardoyo
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Selasa, 1 Oktober 2020</small>
-            </div>
+            <h5 class="card-title">Laporan Medical Check Up</h5>
+            <p class="card-text" style="margin-bottom:-0.1%">Berikut adalah hasil check up : </p>
+            <p class="card-text" style="margin-bottom:-0.1%">Nama : Ahmad Zakiy Mulyanto</p>
+            <p class="card-text" style="margin-bottom:-0.1%">Usia : 21 Tahun</p>
+            <p class="card-text" style="margin-bottom:-0.1%">No. KTP : 12345678</p>
+            <p class="card-text" style="margin-bottom:1%">No. Surat : 1</p>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+              <i class="fas fa-download fa-sm text-white-50"></i> Download
+            </a>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Selasa, 1 Oktober 2020</small>
           </div>
         </div>
       </div>
     </div>
-    <!-- Content Row -->
+  </div>
+  <!-- Content Row -->
 
-  </div styles="row">
-  <!-- Footer -->
-  <footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-      <div class="copyright text-center my-auto">
-        <span>Copyright &copy; ALIVE 2020</span>
-      </div>
+</div styles="row">
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright &copy; ALIVE 2020</span>
     </div>
-  </footer>
+  </div>
+</footer>
 </div>
 <!-- End of Footer -->
 @endsection
