f77da97829fd59288782cf10285253f30161143d
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-09 02:16:08 +0700

    chart pasien

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -9,13 +9,14 @@ class ChartController extends Controller
     public function index()
     {
         $dataset = \App\Dataset::all();
-        $data1=[];
-        $data2=[];
-        foreach($dataset as $data){
-            $data1=$data->data1;
-            $data2=$data->data2;
+        $data1 = [];
+        $data2 = [];
+        foreach ($dataset as $data) {
+            $data1 = $data->data1;
+            $data2 = $data->data2;
         }
-        return view('chart', ['dataset' => $dataset], ['data1' => $data1],['data2' => $data2]);
+        dd{
+            $data1};
+        return view('chart', ['dataset' => $dataset], ['data1' => $data1], ['data2' => $data2]);
     }
-    //
 }
