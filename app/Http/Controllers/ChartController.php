d9621a3d610d24479a4f07569538153ec8de1ddc
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-09 02:49:21 +0700

    debug

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -13,12 +13,12 @@ class ChartController extends Controller
         $data1 = '';
         $data2 = '';
         while ($row = mysqli_fetch_array($dataset)) {
-            dd(json_encode($dataset));
             $data1 = $data1 . '"' . $row['data1'] . '",';
             $data2 = $data2 . '"' . $row['data2'] . '",';
         }
         $data1 = trim($data1, ",");
         $data2 = trim($data2, ",");
+        dd(json_encode($data1));
 
 
         return view('chart', ['dataset' => $dataset], ['data1' => $data1], ['data2' => $data2]);
