a6fb1a2cf31cb19bebb66a762ffaf30caa1bff49
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-09 02:54:11 +0700

    debug

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -9,17 +9,13 @@ class ChartController extends Controller
     public function index()
     {
         $dataset = \App\Dataset::all();
-
-        $data1 = '';
-        $data2 = '';
-        while ($row = count($dataset)) {
-            $data1 = $data1 . '"' . $row['data1'] . '",';
-            $data2 = $data2 . '"' . $row['data2'] . '",';
+        $data1 = "";
+        $data2 = "";
+        foreach ($dataset as $data) {
+            $data1 = $data->data1;
+            // $data2 = $data->data2;
         }
-        $data1 = trim($data1, ",");
-        $data2 = trim($data2, ",");
         dd(json_encode($data1));
-
 
         return view('chart', ['dataset' => $dataset], ['data1' => $data1], ['data2' => $data2]);
     }
