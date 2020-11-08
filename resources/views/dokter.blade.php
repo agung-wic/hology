d2449f8813d959159b656d6ba7516de56bfe614b
mpuhambyah <mpu.hambyah@gmail.com>
2020-11-09 03:46:03 +0700

    tampilan

--- resources/views/dokter.blade.php
+++ resources/views/dokter.blade.php
@@ -11,14 +11,26 @@
     </a>
 
     <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+    <hr class="sidebar-divider my-2">
+
+    <div class="sidebar-heading">
+        Pasien
+    </div>
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
         <a class="nav-link" href="/pasien">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard Pasien</span></a>
+
     </li>
+
+    <hr class="sidebar-divider my-2">
+
+    <div class="sidebar-heading">
+        Pasien
+    </div>
+
     <li class="nav-item active">
         <a class="nav-link" href="/dokter">
             <i class="fas fa-fw fa-tachometer-alt"></i>
